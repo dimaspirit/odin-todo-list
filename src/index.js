0f44c6dac93769172b827085e3a5c600972575fc
Dmytro Lobanov <dspwww@gmail.com>
2025-04-21 17:39:47 +0100

    feat: Add a project and a Todo structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log('Hello');
+const Todo = ({title, description, dueDate, priority, projectId} = {}) => {
+  return {
+    title,
+    description,
+    dueDate,
+    priority,
+    projectId,
+  }
+}
+
+class Project {
+  #uuid = crypto.randomUUID();
+
+  constructor(name, description = '') {
+    this.name = name;
+    this.description = description;
+  }
+
+  get id() {
+    return this.#uuid;
+  }
+}
+
+const home = new Project('Home');
+const work = new Project('Work');
+const defaultProject = new Project('Unassigned');
+
+const todo = Todo({title: 'Swith on the computer', projectId: work.id});
+console.log(todo);
+
