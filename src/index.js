e3df8de1cb5a335ce2a11d3505d80846a08bc7d9
Dmytro Lobanov <dspwww@gmail.com>
2025-04-21 23:42:12 +0100

    feat: Add AppUI and renderNav funcs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./styles/base.css";
+
 const Todo = ({title, description, dueDate, priority, projectId} = {}) => {
   return {
     title,
@@ -21,10 +23,41 @@ class Project {
   }
 }
 
-const home = new Project('Home');
-const work = new Project('Work');
+class UI {
+  constructor(containerQuery) {
+    this.containerEl = document.querySelector(containerQuery);
+    this.navEl = this.containerEl.querySelector('#nav');
+  }
+
+  renderNavItems(project) {
+    const liEl = document.createElement('li');
+    const buttonEl = document.createElement('button');
+    buttonEl.dataset.id = project.id;
+    buttonEl.textContent = project.name;
+
+    liEl.appendChild(buttonEl);
+    return liEl;
+  }
+
+  renderNav(projects) {
+    const ulEl = document.createElement('ul');
+    const buttonNewEl = document.createElement('button');
+    buttonNewEl.textContent = `${String.fromCodePoint('0x1F680')} New Project`;
+
+    projects.forEach(project => {
+      ulEl.appendChild(this.renderNavItems(project));
+    });
+
+    this.navEl.appendChild(ulEl);
+    this.navEl.appendChild(buttonNewEl);
+  }
+}
+
+const appUI = new UI('.container');
+
+const workProject = new Project('Work');
 const defaultProject = new Project('Unassigned');
 
-const todo = Todo({title: 'Swith on the computer', projectId: work.id});
-console.log(todo);
+appUI.renderNav([workProject, defaultProject]);
 
+const todo = Todo({title: 'Swith on the computer', projectId: workProject.id});
